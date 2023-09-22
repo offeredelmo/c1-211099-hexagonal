9f84955ec7451d4f5b601ef4896a01b5c7a5fc43
211099 <211099@ids.upchiapas.edu.mx>
2023-09-21 22:53:28 -0600

    se definio la entidad user y se creo el repositori

---
+++ src/user/domain/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    constructor(
+    id: string,
+    name: string,
+    last_name: string, 
+    email: string,
+    password: string,
+    loan_status: boolean,
+
+    ){}
+     
+
+}
---
+++ src/user/domain/userRepository.ts
@@ -0,0 +1,14 @@
+import { User } from "./user";
+
+export interface IUsuarioRepositorio {
+    listAllUsers(users: User[]): Promise<User[] | null>
+    listAllUsersInactive(status: boolean ): Promise<User[] | null>
+    listUserByAttribute(attribute: string): Promise<User[] | null>//posiblemente cambiarlo a int 1:email 2: nombre 3: celular
+    getUserById(id:number): Promise<User | null>;
+    addUser(user: User): Promise<void>;
+    updateUser(user: User): Promise<void>;
+    updatePasswordr(id: string, password: String): Promise<void>;
+    deleteUser(id: string):  Promise<void>;
+    activateUser(onOff: boolean ):  Promise<void>;
+}
+
