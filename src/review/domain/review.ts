5914744a085bc4554c17942ff28d65132b5ce12c
211099 <211099@ids.upchiapas.edu.mx>
2023-09-22 00:04:09 -0600

    se definio la entidad de review y su repository

---
+++ src/review/domain/review.ts
@@ -0,0 +1,12 @@
+export class Review {
+    constructor(
+        id: string,
+        id_user: string,
+        id_book: string,
+        loan_date: Date,
+        deadline: Date,
+        status: boolean,
+    ) {
+        
+    }
+}
