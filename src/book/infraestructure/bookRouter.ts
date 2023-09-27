a835ffa9325895be04e007bf7fb36914845ecd7a
211099 <211099@ids.upchiapas.edu.mx>
2023-09-27 01:24:24 -0600

    eliminacion de las validaciones para cambiarlas por class-validator

--- src/book/infraestructure/bookRouter.ts
+++ src/book/infraestructure/bookRouter.ts
@@ -1,24 +1,24 @@
 import express from "express";
 import { activateBookController, addBookController, deleteBookController, getBookByIdController, listAllBooksController, listAllBooksInactiveController, listByFilterController, listIfReviewController, updateBookController } from "./dependecies";
-import { validationAddBook, validationUpdateBook, validationUuid, validationListByFilter } from "./validators/books";
+
 
 export const bookRoutes = express.Router();
 
 bookRoutes.get('/',listAllBooksController.run.bind(listAllBooksController))
 
-bookRoutes.post('/',validationAddBook,addBookController.run.bind(addBookController))
+bookRoutes.post('/',addBookController.run.bind(addBookController))
 
 bookRoutes.get('/inactive',listAllBooksInactiveController.run.bind(listAllBooksInactiveController))
 
-bookRoutes.put("/",validationUpdateBook,updateBookController.run.bind(updateBookController))
+bookRoutes.put("/",updateBookController.run.bind(updateBookController))
 
-bookRoutes.delete("/",validationUuid,deleteBookController.run.bind(deleteBookController))
+bookRoutes.delete("/",deleteBookController.run.bind(deleteBookController))
 
-bookRoutes.get("/id",validationUuid,getBookByIdController.run.bind(getBookByIdController))
+bookRoutes.get("/id",getBookByIdController.run.bind(getBookByIdController))
 
-bookRoutes.put("/id",validationUuid,activateBookController.run.bind(activateBookController))
+bookRoutes.put("/id",activateBookController.run.bind(activateBookController))
 
-bookRoutes.get("/filter",validationListByFilter,listByFilterController.run.bind(listByFilterController))
+bookRoutes.get("/filter",listByFilterController.run.bind(listByFilterController))
 
 bookRoutes.get("/whit/review",listIfReviewController.run.bind(listIfReviewController))
 
