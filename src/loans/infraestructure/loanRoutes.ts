457a65f564a92f92ec2296e70669bed10276ac45
211099 <211099@ids.upchiapas.edu.mx>
2023-09-29 12:43:26 -0600

    agregar la verificacion del token

--- src/loans/infraestructure/loanRoutes.ts
+++ src/loans/infraestructure/loanRoutes.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import { listAllLoansController, returnLoanController, userLoanBookController } from "./dependencies";
+import { validateToken } from "../../helpers/verifyToken";
 
 
 
 export const loanRoutes = express.Router();
 
 
-loanRoutes.post('/',userLoanBookController.run.bind(userLoanBookController))
-loanRoutes.put('/',returnLoanController.run.bind(returnLoanController))
-loanRoutes.get('/',listAllLoansController.run.bind(listAllLoansController))
+loanRoutes.post('/',validateToken ,userLoanBookController.run.bind(userLoanBookController))
+loanRoutes.put('/',validateToken ,returnLoanController.run.bind(returnLoanController))
+loanRoutes.get('/',validateToken ,listAllLoansController.run.bind(listAllLoansController))
 
 
 
