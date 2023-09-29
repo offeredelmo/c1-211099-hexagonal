a8de5ebeda901d6c250362d2b41abe9f6f64ea69
211099 <211099@ids.upchiapas.edu.mx>
2023-09-29 12:58:40 -0600

    se agrego la validacion del token

---
+++ src/helpers/veryfyToken.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { verifyToken } from './token';
+
+
+export const validateToken = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization) {
+        return res.status(400).send({ error: "No hay token" });
+    }
+    
+    const token = req.headers.authorization.split(' ').pop() as string;
+
+    if (!token) {
+        return res.status(400).send({ error: "Token no encontrado" });
+    }
+    console.log("aaa")
+    const tokenData = await verifyToken(token);
+    console.log("eee")
+  
+    if (tokenData && tokenData.uuid) {
+        next();
+    } else {
+        return res.status(401).send({ error: "Token inválido" });
+    }
+};
--- src/review/infrestructure/reviewRoutes.ts
+++ src/review/infrestructure/reviewRoutes.ts
@@ -1,17 +1,20 @@
 import express from "express";
 import { activateReviewController, addReviewUsController, deleteReviewController, getReviewByIdController, inactiveteReviewController, listAllReviewsController, listReviewByUserController, listReviewsInactiveController, updateReviewController } from "./dependecies";
+import { validateToken } from "../../helpers/veryfyToken";
+
+
 
 export const reviewRouter = express.Router();
 
-reviewRouter.post('/',addReviewUsController.run.bind(addReviewUsController))
-reviewRouter.get('/',listAllReviewsController.run.bind(listAllReviewsController))
-reviewRouter.get('/user',listReviewByUserController.run.bind(listReviewByUserController))
-reviewRouter.get('/review',getReviewByIdController.run.bind(getReviewByIdController))
-reviewRouter.get('/list/inactive',listReviewsInactiveController.run.bind(listReviewsInactiveController))
-reviewRouter.delete('/',deleteReviewController.run.bind(deleteReviewController))
-reviewRouter.put('/',updateReviewController.run.bind(updateReviewController))
-reviewRouter.put('/inactivate',inactiveteReviewController.run.bind(inactiveteReviewController))
-reviewRouter.put('/activate',activateReviewController.run.bind(activateReviewController))
+reviewRouter.post('/',validateToken ,addReviewUsController.run.bind(addReviewUsController))
+reviewRouter.get('/',validateToken ,listAllReviewsController.run.bind(listAllReviewsController))
+reviewRouter.get('/user',validateToken ,listReviewByUserController.run.bind(listReviewByUserController))
+reviewRouter.get('/review',validateToken  ,getReviewByIdController.run.bind(getReviewByIdController))
+reviewRouter.get('/list/inactive',validateToken ,listReviewsInactiveController.run.bind(listReviewsInactiveController))
+reviewRouter.delete('/',validateToken ,deleteReviewController.run.bind(deleteReviewController))
+reviewRouter.put('/',validateToken ,updateReviewController.run.bind(updateReviewController))
+reviewRouter.put('/inactivate',validateToken ,inactiveteReviewController.run.bind(inactiveteReviewController))
+reviewRouter.put('/activate',validateToken ,activateReviewController.run.bind(activateReviewController))
 
 
 
